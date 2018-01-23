afbd74f74fe94d6f257d580611afb6594c2c2613
DTerblanche <DTerblanche@DTER-NB-01>
2018-01-23 08:18:26 +0200

    Added Initial Middle API

---
+++ MiddleWearAPI/MiddleWearAPI/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace MiddleWearAPI.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ MiddleWearAPI/MiddleWearAPI/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MiddleWearAPI.WebApiApplication" Language="C#" %>
